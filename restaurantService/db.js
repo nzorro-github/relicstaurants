b577bb038b97a29e6fd2e019251a1eadadd7435a
Nzorro <nzojihipolito@gmail.com>
2025-08-10 09:07:12 -0500

    some fixes to mongo client due to new version

--- restaurantService/db.js
+++ restaurantService/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ObjectId } = require('mongodb');
-
+var RestaurantRecord = require('./model').Restaurant;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://restaurant-mongo-svc:27017/restaurantdb';
 let client = null;
 
@@ -16,41 +16,48 @@ class MongoStorage {
     }
 
     async _connect() {
-        if (!client.isConnected()) {
+        if (!client.topology || !client.topology.isConnected()) {
             await client.connect();
         }
     }
 
     async getAll() {
         await this._connect();
-        const items = await this.collection.find().toArray();
-        return items.map(item => ({ id: item._id.toString(), ...item, _id: undefined }));
+        const items = await this.collection.find().toArray()[0];
+	    
+  	var restaurantItems = [];
+	if(items) {
+            console.log(items);
+	    items.forEach((item, i) => {
+		const record =  { ...item };
+	        console.log(`Item ${i}: ${item}, record=${record} `);
+	        restaurantItems.push(new RestaurantRecord(record));
+	    });
+	}
+	    
+        return restaurantItems;
     }
 
     async add(data) {
         await this._connect();
         const item = { ...data };
-        if (!item.id) {
-            item._id = new ObjectId();
-        } else {
-            item._id = ObjectId(item.id);
-            delete item.id;
-        }
+        
         await this.collection.insertOne(item);
-        return { id: item._id.toString(), ...item, _id: undefined };
+        return ;
     }
 
     async getById(id) {
         await this._connect();
-        const item = await this.collection.findOne({ _id: ObjectId(id) });
-        return item ? { id: item._id.toString(), ...item, _id: undefined } : null;
+        const item = await this.collection.findOne({ id: id });
+	// item._id = null;
+        return item ? {  ...item } : null;
     }
 
     async deleteById(id) {
         await this._connect();
-        const result = await this.collection.deleteOne({ _id: ObjectId(id) });
+        const result = await this.collection.deleteOne({ id: id });
         return result.deletedCount > 0;
     }
 }
 
-exports.MongoStorage = MongoStorage;
+exports.Mongo = MongoStorage;
